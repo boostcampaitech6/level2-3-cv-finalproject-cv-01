7a5dd2f279ee29e7bc0e7dd58e3061919ff80443
Jongwook Bae <bjonguk@gmail.com>
2024-02-20 11:59:41 +0900

    feat: candle matching 메뉴화 및 prediction 수정 (#32)
    * feat: candle matching 메뉴화 및 prediction 수정
    
    #30
    
    * feat: candle matching 오류 수정
    
    #30

--- streamlit/demo_site.py
+++ streamlit/demo_site.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from streamlit_option_menu import option_menu  # 추가
 # 페이지 모듈 임포트
-from views import home, dashboard, prediction, about_us
-
+from views import home, dashboard, prediction, candle_matching, about_us
+st.set_page_config(layout="wide")
 # 옵션 메뉴를 사이드바에 추가
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", "Dashboard", "Prediction", "About Us"],
-                           icons=["house", "layout-dashboard", "bar-chart-line", "person lines fill"],
+    selected = option_menu("Main Menu", ["Home", "Dashboard", "Prediction", "Candle Matching", "About Us"],
+                           icons=["house", "bi-clipboard-check", "bi-graph-up-arrow", "bar-chart-line", "person lines fill"],
                            menu_icon="cast", default_index=0,
+                        #    icons: https://icons.getbootstrap.com/
                            styles={
                                "container": {"padding": "5!important", "background-color": "#fafafa"},
                                "icon": {"color": "orange", "font-size": "25px"}, 
@@ -22,5 +23,7 @@ elif selected == "Dashboard":
     dashboard.app()
 elif selected == "Prediction":
     prediction.app()
+elif selected == "Candle Matching":
+    candle_matching.app()
 elif selected == "About Us":
     about_us.app()
