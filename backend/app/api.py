957245794eff7594249e79bf1c742fb7e0b59a7a
2018007956 <bbmt1906@hanyang.ac.kr>
2024-02-27 18:35:19 +0900

    dev: backend baseline
    #53

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,55 +1,7 @@
-from fastapi import APIRouter, HTTPException, status
-from sqlmodel import Session, select
-from .schemas import PredictionRequest, PredictionResponse
-from .database import PredictionResult, engine
-from model import get_CNN5d_5d, inference
-from utils import get_stock_data
+from fastapi import FastAPI
 
-router = APIRouter()
+app = FastAPI()
 
-
-@router.post("/stock", tags=["stock"])
-async def stock_df(stock_info: PredictionRequest):
-    df = get_stock_data(stock_info.name, stock_info.period, stock_info.interval)
-    return df.to_json(orient="records")
-
-@router.post("/pred/cnn", tags=["predict"])
-def pred_cnn(stock_info: PredictionRequest)->PredictionResponse:
-
-    src_window = 5
-    df = get_stock_data(stock_info.name, '1mo', '1d') # period: 1mo, interval: 1d
-    df = df.iloc[-src_window:]
-    model = get_CNN5d_5d()
-
-    pred = inference(model, df, src_window)
-
-    # 예측 결과를 DB에 저장
-    prediction_result = PredictionResult(result=pred['pred_idx'], percentage=pred['percentage'])
-    with Session(engine) as session:
-        session.add(prediction_result)
-        session.commit()
-        session.refresh(prediction_result)
-    
-    return PredictionResponse(id=prediction_result.id, result=prediction_result.result, percentage=prediction_result.percentage)
-
-@router.get("/pred/cnn")
-def get_predictions() -> list[PredictionResponse]:
-    with Session(engine) as session:
-        statement = select(PredictionResult)
-        prediction_results = session.exec(statement).all()
-        return [
-            PredictionResponse(id=prediction_result.id, result=prediction_result.result, percentage=prediction_result.percentage)
-            for prediction_result in prediction_results
-        ]
-
-@router.get("/pred/cnn/{id}")
-def get_preidction(id: int) -> PredictionResponse:
-    with Session(engine) as session:
-        prediction_result = session.get(PredictionResult, id)
-        if not prediction_result:
-            raise HTTPException(
-                detail="Not found", status_code=status.HTTP_404_NOT_FOUND
-            )
-        return PredictionResponse(
-            id=prediction_result.id, result=prediction_result.result, percentage=prediction_result.percentage
-        )
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
