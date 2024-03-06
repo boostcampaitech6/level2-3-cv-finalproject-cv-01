fb480db5c8a848f012a06466bae67b6185d5c2c4
Chaea Kim <chaea11s0@gmail.com>
2024-03-06 21:16:37 +0900

    feat: DB 구축 (#68)
    * feat: save model prediction results to db
    
    #66
    
    * fix: add model's pred_idx (the result of a rise/fall) to db and response
    
    #66

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,25 @@
-import uvicorn
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from loguru import logger 
+from sqlmodel import SQLModel
 
+# from app.config import config
+from app.database import engine
+from app.api import router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("creating database table")
+    SQLModel.metadata.create_all(engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
