390116c25065e2505b843dc658b1f2b625bad5bc
sihyun <classaen7@naver.com>
2024-03-10 22:02:34 +0900

    fix: model call
    #86

--- model/train.py
+++ model/train.py
@@ -26,7 +26,7 @@ def main(config):
     valid_data_loader = data_loader.split_validation()
 
     # build model architecture, then print to console
-    model = config.init_obj('arch', module_arch, 20)
+    model = config.init_obj('arch', module_arch, config['data_loader']['args']['window_size'])
     logger.info(model)
 
     # prepare for (multi-device) GPU training
