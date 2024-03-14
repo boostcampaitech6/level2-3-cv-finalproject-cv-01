e9e2889d63542fb281f32a84ff867025a80a3f1e
2018007956 <bbmt1906@hanyang.ac.kr>
2024-03-14 14:00:43 +0000

    feat: implement db schema
    #96

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,55 @@
-import datetime
+from datetime import datetime
 from sqlmodel import SQLModel, Field, create_engine
 from .config import config
 
-class PredictionResult(SQLModel,table=True):
-    id: int = Field(default=None, primary_key=True)
-    result: int
-    percentage: float
-    created_at: str = Field(default_factory=datetime.datetime.now)
+class UserInfo(SQLModel,table=True):
+    id: int = Field(default=None, primary_key=True) # None: guest user
+    created_at: str = Field(default_factory=datetime.now)
+
+class FavoriteStocks(SQLModel,table=True):
+    user_id: int 
+    stock_code: int = Field(primary_key=True)
+
+class SavePredResults(SQLModel,table=True):
+    user_id: int 
+    stock_code: int = Field(primary_key=True)
+    date: str
+    pred_1day_result: int
+    pred_1day_percent: float
+    pred_2day_result: int
+    pred_2day_percent: float
+    pred_3day_result: int
+    pred_3day_percent: float
+    pred_4day_result: int
+    pred_4day_percent: float
+    pred_5day_result: int
+    pred_5day_percent: float
+    pred_6day_result: int
+    pred_6day_percent: float
+    pred_7day_result: int
+    pred_7day_percent: float
+
+class KRX(SQLModel,table=True):
+    stock_code: str = Field(primary_key=True)
+    stock_name: str
+
+class CNNPredHistory(SQLModel,table=True):
+    stock_code: int = Field(primary_key=True)
+    date: str = Field(primary_key=True)
+    close: str
+    pred_1day_result: int
+    pred_1day_percent: float
+    pred_2day_result: int
+    pred_2day_percent: float
+    pred_3day_result: int
+    pred_3day_percent: float
+    pred_4day_result: int
+    pred_4day_percent: float
+    pred_5day_result: int
+    pred_5day_percent: float
+    pred_6day_result: int
+    pred_6day_percent: float
+    pred_7day_result: int
+    pred_7day_percent: float
     
 engine = create_engine(config.db_url)
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,9 +1,51 @@
 from pydantic import BaseModel
+from datetime import datetime
 
-class PredictionRequest(BaseModel):
+class UserInfoResponse(BaseModel):
+    id: int
+    created_at: datetime
+
+class FavoriteStocksResponse(BaseModel):
+    user_id: int
+    stock_code: int
+
+class SavePredResultsResponse(BaseModel):
+    user_id: int
+    stock_code: int
+    date: str
+    pred_1day_result: int
+    pred_1day_percent: float
+    pred_2day_result: int
+    pred_2day_percent: float
+    pred_3day_result: int
+    pred_3day_percent: float
+    pred_4day_result: int
+    pred_4day_percent: float
+    pred_5day_result: int
+    pred_5day_percent: float
+    pred_6day_result: int
+    pred_6day_percent: float
+    pred_7day_result: int
+    pred_7day_percent: float
+
+class KRXResponse(BaseModel):
+    code: str 
     name: str
 
-class PredictionResponse(BaseModel):
-    id: int
-    result: int
-    percentage: float
+class CNNPredResponse(BaseModel):
+    stock_code: int 
+    date: str 
+    pred_1day_result: int
+    pred_1day_percent: float
+    pred_2day_result: int
+    pred_2day_percent: float
+    pred_3day_result: int
+    pred_3day_percent: float
+    pred_4day_result: int
+    pred_4day_percent: float
+    pred_5day_result: int
+    pred_5day_percent: float
+    pred_6day_result: int
+    pred_6day_percent: float
+    pred_7day_result: int
+    pred_7day_percent: float
