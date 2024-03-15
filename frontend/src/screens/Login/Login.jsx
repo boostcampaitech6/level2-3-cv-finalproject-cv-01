8717ddbc1df9359958f3e0d33d5c1dd1dda1c2ea
JasonPark <cjpark137@naver.com>
2024-03-15 17:18:04 +0900

    feat: kakao login init
    #90

--- frontend/src/screens/Login/Login.jsx
+++ frontend/src/screens/Login/Login.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./style.css";
 
 export const Login = () => {
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.REACT_APP_KAKAO_REDIRECT_URI)}&response_type=code`;
+
   return (
     <div className="login">
       <div className="div-3">
-        <Link className="text" to="/home">
+        <a href="/home" className="text">
           <div className="text-wrapper-12">로그인 없이 체험하기</div>
-        </Link>
-        <Link className="button-kakao-login" to="/login-kakao">
+        </a>
+        <a href={KAKAO_AUTH_URL} className="button-kakao-login">
           <img className="kakao-logo" alt="Kakao logo" src="/img/2x.png" />
           <div className="container">
             <div className="label">카카오 로그인</div>
           </div>
-        </Link>
+        </a>
         <div className="frame-demo">
           <div className="text-2">
             <div className="text-wrapper-13">데모 영상</div>
