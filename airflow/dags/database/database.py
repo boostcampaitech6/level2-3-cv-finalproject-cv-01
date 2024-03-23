2d7f7aab527bd5eff3a1ac06897959f2416698b5
sion <79098475+classaen7@users.noreply.github.com>
2024-03-24 01:25:14 +0900

    fix: database.py

--- airflow/dags/database/database.py
+++ airflow/dags/database/database.py
@@ -2,8 +2,8 @@ import pymysql
 import pymysql.cursors
 
 user_name = 'admin'
-pass_my = 'mysqlpw1'
-host_my = '비밀번호 기입'
+pass_my = '비밀번호 기입'
+host_my = '공인 IP'
 db_name = 'airflow_test_db'
 
 connection = pymysql.connect(host=host_my,
