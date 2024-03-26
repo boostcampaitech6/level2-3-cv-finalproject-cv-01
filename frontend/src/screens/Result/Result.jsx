cd5caedf06c01f011db4f9a22fba8d4744ef1bfb
minyun-e <136993865+minyun-e@users.noreply.github.com>
2024-03-26 18:34:48 +0900

    feat: add gauge chart (#121)
    #120

--- frontend/src/screens/Result/Result.jsx
+++ frontend/src/screens/Result/Result.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Heart } from "../../components/Heart";
 import { Menu } from "../../components/Menu";
@@ -10,8 +9,103 @@ import { useParams, useLocation } from "react-router-dom";
 import { AdvancedRealTimeChart, SymbolInfo } from 'react-ts-tradingview-widgets';
 import React, { useState, useEffect } from 'react';
 
+import { Radar } from 'react-chartjs-2';
+import { Chart as ChartJS } from 'chart.js/auto';
+import { ChartOptions } from 'chart.js';
+
+import GaugeChart from 'react-gauge-chart'
+
+// 차트 데이터
+const scores = [1, 2, 3, 4, 5,6];
+
+// 차트 옵션
+const COLOR = {
+  ORANGE_1: 'rgba(255, 108, 61, 1)',  
+  GRAY_9E: 'rgba(158, 158, 158, 1)',  
+  BLACK: '#000000'                    
+};
 
 export const Result = () => {
+  // 애니메이션을 제어하기 위한 상태
+  const [percent, setPercent] = useState(0.4);
+
+  // 클릭 이벤트를 처리하는 함수
+  const handleClick = () => {
+    // 애니메이션을 재시작하기 위해 percent를 0으로 설정하고,
+    // 바로 이어서 원래 값으로 되돌림
+    setPercent(0);
+    setTimeout(() => setPercent(0.6), 0);
+  };
+
+
+  // 차트 데이터
+  const chartData = {
+    labels: ['CNN', 'HMM', 'AR', 'News', ['Candle   ', 'Matching'], 'LSTM'],
+    datasets: [
+      {
+        label: '팀 점수',
+        data: scores,
+        backgroundColor: 'rgba(255, 108, 61, 0.2)',
+      },
+    ],
+  };
+
+  // 차트 옵션
+  const chartOptions = {
+      elements: {
+        //데이터 속성.
+        line: {
+          borderWidth: 2,
+          borderColor: COLOR.ORANGE_1,
+        },
+        //데이터 꼭짓점.
+        point: {
+          pointBackgroundColor: COLOR.ORANGE_1,
+        },
+      },
+      scales: {
+        r: {
+          ticks: {
+            stepSize: 2.5,
+            display: false,
+          },
+          grid: {
+            color: COLOR.GRAY_9E,
+          },
+          //라벨 속성 지정.
+          pointLabels: {
+            font: {
+              size: 16,
+              weight: 'bold',
+              family: "Noto Sans KR",
+            },
+            color: COLOR.BLACK,
+          },
+          angleLines: {
+            display: false,
+          },
+          suggestedMin: 0,
+          suggestedMax: 10,
+        },
+      },
+      //위에 생기는 데이터 속성 label 타이틀을 지워줍니다.
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      //기본 값은 가운데에서 펴져나가는 애니메이션 형태입니다.
+      animation: {
+        duration: 0,
+      },
+      layout: {
+        padding: {
+          right: 30, // 왼쪽 여백을 20px로 설정
+        },
+      },
+    
+    };
+
   const { symbol } = useParams(); // URL 파라미터에서 symbol 값을 가져옵니다.
   const location = useLocation();
   const { stockLabel } = location.state || {};
@@ -36,6 +130,34 @@ export const Result = () => {
     <div className="result">
       <div className="frame-11">
         <div className="content-7">
+        <div className="radar-chart-container">
+          <div className='radar-chart-color'>
+            <Radar data={chartData} options={chartOptions} />
+            </div>
+          </div>
+          <div className="gauge-chart-container clickable-cursor" onClick={handleClick}>
+            <GaugeChart id="gauge-chart3" 
+              animate={true}
+              hideText={true}
+              nrOfLevels={5}
+              cornerRadius={0}
+              arcWidth={0.06}
+              arcPadding={0.015}
+              percent={0.6}
+              textColor="#3C3C3C"
+              needleColor="#7d49f5"
+              needleBaseColor="#4616B5"
+              colors={["#DF5341", "#782A2B", "#42464F", "#1F3A82","#3764F3" ]}
+            />
+            <div className="gauge-labels">
+              <span className="gauge-label left">STRONG<br />SELL</span>
+              <span className="gauge-label left2">SELL</span>
+              <span className="gauge-label middle">NEUTRAL</span>
+              <span className="gauge-label right2">BUY</span>
+              <span className="gauge-label right">STRONG<br /> BUY</span>
+            </div>
+          </div>
+          
           <div className="info-4">
             <SymbolInfo
               colorTheme="light"
