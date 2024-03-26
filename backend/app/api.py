18201c3905f249d1323c8dd73d6d9466fd88696f
Chaea Kim <chaea11s0@gmail.com>
2024-03-26 18:32:51 +0900

    feat: WAS서버와 DB서버 분리 (#112)
    * feat: implement db schema
    
    #96
    
    * fix: rename functions to maintain consistency
    
    #96
    
    * fix: the input of AI result saving function
    the model prediction value is obtained from CNNPredHistory table
    #96
    
    * fix: modify stock_code type from int to str
    
    #96
    
    * fix: the input stock_code type of save_user_favorite
    int to str
    #96
    
    * feat: mysql server connect
    Separation of API and DB Server
    #96
    
    * feat: add mysql packages
    
    #70
    
    * remove: create db w/ lifespan
    
    cause using MySQL DB
    #96
    
    * feat: using env variables to manage sensitive info. such as password
    
    #96
    
    * remove: useless code
    
    #96
    
    * remove: model inference files for storing in db
    model inference will be performed in Airflow
    #100
    
    * remove: save stock info API
    This will be handled in Airflow
    #96
    
    * feat: model prediction call API
    
    #96
    
    * fix: set db info to be connected
    
    #96
    
    * fix: date type from str to datetime
    
    #96
    
    * refactor: move news API from main to app/api
    
    #96
    
    * fix: poetry update

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,12 +1,29 @@
 from fastapi import APIRouter, HTTPException, status
 from sqlalchemy.exc import IntegrityError
 from sqlmodel import Session, select
-from .schemas import UserInfoResponse, FavoriteStocksResponse, SavePredResultsResponse, KRXResponse, CNNPredResponse
-from .database import UserInfo, FavoriteStocks, SavePredResults, KRX, CNNPredHistory, engine
-from .config import config
-from datetime import datetime
+from .schemas import UserInfoResponse, FavoriteStocksResponse, KRXResponse, CNNPredResponse, TimeSeriesPredResponse, BertPredResponse, CandlePredResponse
+from .database import UserInfo, FavoriteStocks, KRX, CNNPredHistory, TimeSeriesPredHistory, BertPredHistory, CandlePredHistory, engine
+from typing import List
+from utils.newsdata import fetch_news_data
 
 router = APIRouter()
+
+@router.get("/stockinfo", tags=["stock"])
+def get_stock_info() -> list[KRXResponse]:
+    with Session(engine) as session:
+        statement = select(KRX)
+        results = session.exec(statement).all()
+        return [
+            KRXResponse(stock_code=result.code, stock_name=result.name, market=result.market)
+            for result in results
+        ]
+
+
+@router.get("/news", response_model=List[dict])
+async def get_news(query: str = "삼성전자"):
+    news_data = await fetch_news_data(query)
+    return news_data
+
 
 @router.post("/user", tags=["user"])
 def save_user_info(user_id: int): 
@@ -90,158 +107,52 @@ def get_user_favorite(user_id: int) -> list[FavoriteStocksResponse]:
         ]
 
 
-@router.post("/user/saved/{user_id}", tags=["user"])
-def save_user_saved(user_id: int, stock_code: str): # if press saved button
+@router.get("/pred/cnn", tags=["predict"])
+def get_cnn_pred() -> list[CNNPredResponse]:
     with Session(engine) as session:
-        # user_id가 UserInfo에 저장되어 있는 값인지 확인
-        user_info = session.query(UserInfo).filter(UserInfo.id == user_id).first()
-        if not user_info:
-            # `UserInfo`에 `user_id`가 없으면 404 에러를 반환
-            raise HTTPException(detail="User not found", status_code=status.HTTP_404_NOT_FOUND)
-
-        # stock_code가 CNNPredResponse 저장되어 있는 값인지 확인
-        pred_info = session.query(CNNPredHistory).filter(CNNPredHistory.stock_code == stock_code).first()
-        if not pred_info:
-            # `UserInfo`에 `user_id`가 없으면 404 에러를 반환
-            raise HTTPException(detail="Prediction result not found", status_code=status.HTTP_404_NOT_FOUND)
-        
-        date = session.query(CNNPredHistory.date).order_by(CNNPredHistory.date.desc()).first()
-        # CNNPredHistory table 조회
-        search = session.query(CNNPredHistory).filter(
-            CNNPredHistory.stock_code == stock_code,
-            CNNPredHistory.date == date.date
-            ).first()
-        # 사용자가 존재하면 AI 분석 결과 저장
-        result = SavePredResults(user_id=user_id, stock_code=stock_code, 
-                    date=date.date,
-                    pred_1day_result=search.pred_1day_result,pred_1day_percent=search.pred_1day_percent,
-                    pred_2day_result=search.pred_2day_result,pred_2day_percent=search.pred_2day_percent,
-                    pred_3day_result=search.pred_3day_result,pred_3day_percent=search.pred_3day_percent,
-                    pred_4day_result=search.pred_4day_result,pred_4day_percent=search.pred_4day_percent,
-                    pred_5day_result=search.pred_5day_result,pred_5day_percent=search.pred_5day_percent,
-                    pred_6day_result=search.pred_6day_result,pred_6day_percent=search.pred_6day_percent,
-                    pred_7day_result=search.pred_7day_result,pred_7day_percent=search.pred_7day_percent)
-        try:
-            session.add(result)
-            session.commit()
-            session.refresh(result)
-        except IntegrityError:
-            session.rollback()
-            print('Existed')
-    return SavePredResultsResponse(user_id=result.user_id, stock_code=result.stock_code, date=result.date,
-                            pred_1day_result=result.pred_1day_result,pred_1day_percent=result.pred_1day_percent,
-                            pred_2day_result=result.pred_2day_result,pred_2day_percent=result.pred_2day_percent,
-                            pred_3day_result=result.pred_3day_result,pred_3day_percent=result.pred_3day_percent,
-                            pred_4day_result=result.pred_4day_result,pred_4day_percent=result.pred_4day_percent,
-                            pred_5day_result=result.pred_5day_result,pred_5day_percent=result.pred_5day_percent,
-                            pred_6day_result=result.pred_6day_result,pred_6day_percent=result.pred_6day_percent,
-                            pred_7day_result=result.pred_7day_result,pred_7day_percent=result.pred_7day_percent)
-
-@router.get("/user/saved/{user_id}", tags=["user"])
-def get_user_saved(user_id: int) -> list[SavePredResultsResponse]:
-    with Session(engine) as session:
-        # UserInfo에 관련 레코드가 있는지 먼저 확인
-        user_exists = session.query(UserInfo).filter(UserInfo.id == user_id).first()
-        if not user_exists:
-            raise HTTPException(
-                detail="User not found", 
-                status_code=status.HTTP_404_NOT_FOUND
-            )
-        
-        # user_id로 SavePredResults에서 관련 레코드를 모두 찾음
-        results = session.query(SavePredResults).filter(SavePredResults.user_id == user_id).all()
-        if not results:
-            raise HTTPException(
-                detail=f"There's no {user_id}'s saved info", status_code=status.HTTP_404_NOT_FOUND
-            )
-        return [
-            SavePredResultsResponse(user_id=result.user_id, stock_code=result.stock_code, date=result.date,
-                pred_1day_result=result.pred_1day_result,pred_1day_percent=result.pred_1day_percent,
-                pred_2day_result=result.pred_2day_result,pred_2day_percent=result.pred_2day_percent,
-                pred_3day_result=result.pred_3day_result,pred_3day_percent=result.pred_3day_percent,
-                pred_4day_result=result.pred_4day_result,pred_4day_percent=result.pred_4day_percent,
-                pred_5day_result=result.pred_5day_result,pred_5day_percent=result.pred_5day_percent,
-                pred_6day_result=result.pred_6day_result,pred_6day_percent=result.pred_6day_percent,
-                pred_7day_result=result.pred_7day_result,pred_7day_percent=result.pred_7day_percent)
-                for result in results
-        ]
-        
-
-@router.post("/stockinfo", tags=["stock"])
-def save_stock_info(): 
-    file = open(config.stock_symbol,'r')
-    file.readline() # drop first row
-    for line in file.readlines():
-        value, label = line.strip().split(',')
-        value = value.split(':')[1]
-        stocks = KRX(stock_code=value, stock_name=label)
-        with Session(engine) as session:
-            session.add(stocks)
-            session.commit()
-            session.refresh(stocks)
-    return KRXResponse(code=stocks.stock_code, name=stocks.stock_name)
-
-@router.get("/stockinfo", tags=["stock"])
-def get_stock_info() -> list[KRXResponse]:
-    with Session(engine) as session:
-        statement = select(KRX)
+        statement = select(CNNPredHistory)
         results = session.exec(statement).all()
         return [
-            KRXResponse(code=result.stock_code, name=result.stock_name)
+            CNNPredResponse(stock_code=result.stock_code, date=result.date, close=result.close,
+                            pred_1day_result=result.pred_1day_result, pred_1day_percent=result.pred_1day_percent,
+                            pred_2day_result=result.pred_2day_result, pred_2day_percent=result.pred_2day_percent,
+                            pred_3day_result=result.pred_3day_result, pred_3day_percent=result.pred_3day_percent,
+                            pred_4day_result=result.pred_4day_result, pred_4day_percent=result.pred_4day_percent,
+                            pred_5day_result=result.pred_5day_result, pred_5day_percent=result.pred_5day_percent,
+                            pred_6day_result=result.pred_6day_result, pred_6day_percent=result.pred_6day_percent,
+                            pred_7day_result=result.pred_7day_result, pred_7day_percent=result.pred_7day_percent)
             for result in results
         ]
-
-
-@router.post("/pred/cnn", tags=["predict"])
-def save_cnn_pred()->CNNPredResponse: 
-    file = open(config.model_pred,'r')
-    file.readline() # drop first row
-    for line in file.readlines():
-        Date,Name,Code,Close,Day_1,Day_2,Day_3,Day_4,Day_5,Day_6,Day_7 = line.strip().split(',')[1:]
-
-        result = CNNPredHistory(
-            stock_code=Code,#KRX.stock_code, 
-            date=Date,#KRX.date,
-            close=Close,
-            pred_1day_result=Day_1.split('/')[0],
-            pred_1day_percent=Day_1.split('/')[1],
-            pred_2day_result=Day_2.split('/')[0],
-            pred_2day_percent=Day_2.split('/')[1],
-            pred_3day_result=Day_3.split('/')[0],
-            pred_3day_percent=Day_3.split('/')[1],
-            pred_4day_result=Day_4.split('/')[0],
-            pred_4day_percent=Day_4.split('/')[1],
-            pred_5day_result=Day_5.split('/')[0],
-            pred_5day_percent=Day_5.split('/')[1],
-            pred_6day_result=Day_6.split('/')[0],
-            pred_6day_percent=Day_6.split('/')[1],
-            pred_7day_result=Day_7.split('/')[0],
-            pred_7day_percent=Day_7.split('/')[1]
-        )
-        with Session(engine) as session:
-            try:
-                session.add(result)
-                session.commit()
-                session.refresh(result)
-            except IntegrityError:
-                session.rollback()
-                print('UNIQUE constraint failed. Check for duplicates in the PK attribute.')
-
-    return CNNPredResponse(
-        stock_code=result.stock_code,
-        date=result.date,
-        pred_1day_result=result.pred_1day_result,
-        pred_1day_percent=result.pred_1day_percent,
-        pred_2day_result=result.pred_2day_result,
-        pred_2day_percent=result.pred_2day_percent,
-        pred_3day_result=result.pred_3day_result,
-        pred_3day_percent=result.pred_3day_percent,
-        pred_4day_result=result.pred_4day_result,
-        pred_4day_percent=result.pred_4day_percent,
-        pred_5day_result=result.pred_5day_result,
-        pred_5day_percent=result.pred_5day_percent,
-        pred_6day_result=result.pred_6day_result,
-        pred_6day_percent=result.pred_6day_percent,
-        pred_7day_result=result.pred_7day_result,
-        pred_7day_percent=result.pred_7day_percent,
-    )
+    
+@router.get("/pred/timeseries", tags=["predict"])
+def get_timeseries_pred(model: str) -> list[TimeSeriesPredResponse]:
+    with Session(engine) as session:
+        results = session.query(TimeSeriesPredHistory).filter(TimeSeriesPredHistory.model == model).all()
+        return [
+            TimeSeriesPredResponse(stock_code=result.stock_code, date=result.date, close=result.close,
+                                   pred_1day=result.pred_1day, pred_2day=result.pred_2day, pred_3day=result.pred_3day, pred_4day=result.pred_4day,
+                                   pred_5day=result.pred_5day, pred_6day=result.pred_6day, pred_7day=result.pred_7day)
+            for result in results
+        ]
+    
+@router.get("/pred/bert", tags=["predict"])
+def get_lstm_pred() -> list[BertPredResponse]:
+    with Session(engine) as session:
+        statement = select(BertPredHistory)
+        results = session.exec(statement).all()
+        return [
+            BertPredResponse(stock_code=result.stock_code, date=result.date,
+                            yesterday_positive=result.yesterday_positive, yesterday_neutral=result.yesterday_neutral, yesterday_negative=result.yesterday_negative,
+                            today_positive=result.today_positive, today_neutral=result.today_neutral, today_negative=result.today_negative)
+            for result in results
+        ]
+    
+@router.get("/pred/candle", tags=["predict"])
+def get_candle_pred() -> list[CandlePredResponse]:
+    with Session(engine) as session:
+        statement = select(CandlePredHistory)
+        results = session.exec(statement).all()
+        return [
+            CandlePredResponse(stock_code=result.stock_code, date=result.date, candle_name=result.candle_name)
+            for result in results
+        ]
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -9,32 +9,15 @@ class FavoriteStocksResponse(BaseModel):
     user_id: int
     stock_code: int
 
-class SavePredResultsResponse(BaseModel):
-    user_id: int
-    stock_code: str
-    date: str
-    pred_1day_result: int
-    pred_1day_percent: float
-    pred_2day_result: int
-    pred_2day_percent: float
-    pred_3day_result: int
-    pred_3day_percent: float
-    pred_4day_result: int
-    pred_4day_percent: float
-    pred_5day_result: int
-    pred_5day_percent: float
-    pred_6day_result: int
-    pred_6day_percent: float
-    pred_7day_result: int
-    pred_7day_percent: float
-
 class KRXResponse(BaseModel):
-    code: str 
-    name: str
+    stock_code: str 
+    stock_name: str
+    market: str
 
 class CNNPredResponse(BaseModel):
     stock_code: str 
-    date: str 
+    date: datetime 
+    close: float	    
     pred_1day_result: int
     pred_1day_percent: float
     pred_2day_result: int
@@ -49,3 +32,30 @@ class CNNPredResponse(BaseModel):
     pred_6day_percent: float
     pred_7day_result: int
     pred_7day_percent: float
+
+class TimeSeriesPredResponse(BaseModel):
+    stock_code: str
+    date: datetime
+    close: str
+    pred_1day: float
+    pred_2day: float
+    pred_3day: float
+    pred_4day: float
+    pred_5day: float
+    pred_6day: float
+    pred_7day: float
+
+class BertPredResponse(BaseModel):
+    stock_code: str
+    date: datetime
+    yesterday_positive: int
+    yesterday_neutral: int
+    yesterday_negative: int
+    today_positive: int
+    today_neutral: int
+    today_negative: int
+
+class CandlePredResponse(BaseModel):
+    stock_code: str
+    date: datetime
+    candle_name: str
