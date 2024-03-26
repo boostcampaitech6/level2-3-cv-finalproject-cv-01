772467bf3cef276014fb93c8053f56e776fc9509
Chaea Kim <chaea11s0@gmail.com>
2024-03-26 18:32:51 +0900

    feat: WAS서버와 DB서버 분리 (#112)
    * feat: implement db schema
    
    #96
    
    * fix: rename functions to maintain consistency
    
    #96
    
    * fix: the input of AI result saving function
    the model prediction value is obtained from CNNPredHistory table
    #96
    
    * fix: modify stock_code type from int to str
    
    #96
    
    * fix: the input stock_code type of save_user_favorite
    int to str
    #96
    
    * feat: mysql server connect
    Separation of API and DB Server
    #96
    
    * feat: add mysql packages
    
    #70
    
    * remove: create db w/ lifespan
    
    cause using MySQL DB
    #96
    
    * feat: using env variables to manage sensitive info. such as password
    
    #96
    
    * remove: useless code
    
    #96
    
    * remove: model inference files for storing in db
    model inference will be performed in Airflow
    #100
    
    * remove: save stock info API
    This will be handled in Airflow
    #96
    
    * feat: model prediction call API
    
    #96
    
    * fix: set db info to be connected
    
    #96
    
    * fix: date type from str to datetime
    
    #96
    
    * refactor: move news API from main to app/api
    
    #96
    
    * fix: poetry update

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,42 +1,25 @@
 from datetime import datetime
-from sqlmodel import SQLModel, Field, create_engine
+from sqlmodel import SQLModel, Field
 from .config import config
+import sqlalchemy
 
 class UserInfo(SQLModel,table=True):
     id: int = Field(default=None, primary_key=True) # None: guest user
-    created_at: str = Field(default_factory=datetime.now)
+    created_at: datetime = Field(default_factory=datetime.now)
 
 class FavoriteStocks(SQLModel,table=True):
-    user_id: int 
+    user_id: int = Field(primary_key=True)
     stock_code: str = Field(primary_key=True)
-
-class SavePredResults(SQLModel,table=True):
-    user_id: int 
-    stock_code: str = Field(primary_key=True)
-    date: str
-    pred_1day_result: int
-    pred_1day_percent: float
-    pred_2day_result: int
-    pred_2day_percent: float
-    pred_3day_result: int
-    pred_3day_percent: float
-    pred_4day_result: int
-    pred_4day_percent: float
-    pred_5day_result: int
-    pred_5day_percent: float
-    pred_6day_result: int
-    pred_6day_percent: float
-    pred_7day_result: int
-    pred_7day_percent: float
 
 class KRX(SQLModel,table=True):
     stock_code: str = Field(primary_key=True)
     stock_name: str
+    market: str
 
 class CNNPredHistory(SQLModel,table=True):
     stock_code: str = Field(primary_key=True)
-    date: str = Field(primary_key=True)
-    close: str
+    date: datetime = Field(primary_key=True)
+    close: float
     pred_1day_result: int
     pred_1day_percent: float
     pred_2day_result: int
@@ -51,5 +34,34 @@ class CNNPredHistory(SQLModel,table=True):
     pred_6day_percent: float
     pred_7day_result: int
     pred_7day_percent: float
-    
-engine = create_engine(config.db_url)
+
+class TimeSeriesPredHistory(SQLModel,table=True):
+    stock_code: str = Field(primary_key=True)
+    date: datetime = Field(primary_key=True)
+    close: float
+    model: str
+    pred_1day: float
+    pred_2day: float
+    pred_3day: float
+    pred_4day: float
+    pred_5day: float
+    pred_6day: float
+    pred_7day: float
+
+class BertPredHistory(SQLModel,table=True):
+    stock_code: str = Field(primary_key=True)
+    date: datetime = Field(primary_key=True)
+    yesterday_positive: int
+    yesterday_neutral: int
+    yesterday_negative: int
+    today_positive: int
+    today_neutral: int
+    today_negative: int
+
+class CandlePredHistory(SQLModel,table=True):
+    stock_code: str = Field(primary_key=True)
+    date: datetime = Field(primary_key=True)
+    candle_name: str
+
+engine = sqlalchemy.create_engine(config.db_url)
+SQLModel.metadata.create_all(engine)
--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()  # 환경 변수 로드
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from loguru import logger 
-from sqlmodel import SQLModel
-
-from typing import List
-from utils.newsdata import fetch_news_data
 from fastapi.middleware.cors import CORSMiddleware
-
-# from app.config import config
-from app.database import engine
 from app.api import router
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    logger.info("creating database table")
-    SQLModel.metadata.create_all(engine)
-    yield
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(router)
 
 app.add_middleware(
@@ -32,16 +19,7 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/news/", response_model=List[dict])
-async def get_news(query: str = "삼성전자"):
-
-    news_data = await fetch_news_data(query)
-    return news_data
-
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
-
-# uvicorn main:app --reload
