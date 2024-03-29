cd1d3c7bb537bdddd7627ba86af511170760d15d
JasonPark <cjpark137@naver.com>
2024-03-29 16:58:15 +0000

    feat: home change, add favorite
    #128

--- frontend/src/components/DivWrapper/DivWrapper.jsx
+++ frontend/src/components/DivWrapper/DivWrapper.jsx
@@ -6,23 +6,23 @@ Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcN
 import React from "react";
 import "./style.css";
 
-export const DivWrapper = ({ className, companyName, graph, symbol, price, change, percentChange, logo }) => {
+export const DivWrapper = ({ className, companyName, graph, symbol, price, change, volume, logo }) => {
   return (
     <div className={`div-wrapper ${className}`}>
       <div className="overlap-group-2">
         <div className="background">
-          <img className="frame-2" alt="Frame" src={graph} />
-          <div className="frame-3">
+          <div className="frame-2">
             <div className="text-wrapper-5">{price}</div>
+          </div>
+          <div className="frame-3">
             <div className="text-wrapper-6">{change}</div>
-            <div className="text-wrapper-7">{percentChange}</div>
+            <div className="text-wrapper-7">{volume}</div>
           </div>
         </div>
         <div className="info-2">
           <div className="text-wrapper-8">{companyName}</div>
           <div className="text-wrapper-9">{symbol}</div>
         </div>
-        <img className="logo-3" alt={`${companyName} logo`} src={logo} />
       </div>
     </div>
   );
