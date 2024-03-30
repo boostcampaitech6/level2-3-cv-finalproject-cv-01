3b0f2bdf5bb61318d2f14c76e49656df54dac90e
sion <79098475+classaen7@users.noreply.github.com>
2024-03-30 21:44:01 +0900

    feat: Airflow 배치 서빙 및 Mysql 주기적 업데이트 (#131)
    * remove: poetry
    
    #129
    
    * remove: related to airflow db init
    
    #127
    
    * feat: change db server
    
    #127
    
    * remove: origin dag files
    
    #127
    
    * rename: (folder) model -> cnn_model
    
    #127
    
    * rename: stock_utils -> utils
    
    #127
    
    * feat: sql query funcs
    
    #127
    
    * feat: update stock db table
    
    #127
    
    * feat: cnn predict dag
    
    #127
    
    * feat: candle pattern match
    
    #127
    
    * feat: sentiment model
    
    #127
    
    * feat: ar model & lstm
    
    #127
    
    * feat: hmm predict
    
    #116
    
    * fix: reduce the message of non-convergence in hmm
    
    #116
    
    * fix: running but not inference
    
    #130
    
    ---------
    
    Co-authored-by: sihyun <classean7@naver.com>
    Co-authored-by: 2018007956 <chaea11s0@gmail.com>

--- airflow/dags/stock_utils/validate.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-import exchange_calendars as ecals
-
-def valid_check(date):
-    # date : 문자열 형태 : %Y-%m-%d
-    XKRX = ecals.get_calendar("XKRX")
-    return XKRX.is_session(date)
