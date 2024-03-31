bd75a3b56fc17b6901e58ca450722721f49f1bd3
minyun-e <enuynim@gmail.com>
2024-03-31 03:50:35 +0000

    feat: change_design
    #124

--- frontend/src/screens/Loading/Loading.jsx
+++ frontend/src/screens/Loading/Loading.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { SyncLoader } from "react-spinners";
-import { Link } from "react-router-dom";
-import { Loding } from "../../components/Loding";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import introVideoDesktop from "../../../dist/video/intro_desktop.mp4";
+import introVideoMobile from "../../../dist/video/intro_mobile.mp4";
 import "./style.css";
 
 export const Loading = () => {
+  const navigate = useNavigate();
+
+  const handleVideoEnd = () => {
+    setTimeout(() => {
+      navigate('/login');
+    }, 800);
+  };
+
   return (
-    <Link className="loading" to="/login">
-      <div className="content-wrapper">
-        <div className="content">
-          <div className="title">
-            <div className="service-name">
-              <div className="t">알려주가</div>
-              <div className="t-2">AI</div>
-            </div>
-            <p className="text">- 쉽고 간편한 AI 주가 예측 서비스</p>
-          </div>
-          <div className="loading-GIF">
-            <SyncLoader color="#7d49f5" />
-            </div>
-        </div>
-      </div>
-    </Link>
+    <div className="video-container">
+      <video autoPlay muted className="video desktop-video" onEnded={handleVideoEnd}>
+        <source src={introVideoDesktop} type="video/mp4" />
+      </video>
+      <video autoPlay muted className="video mobile-video" onEnded={handleVideoEnd}>
+        <source src={introVideoMobile} type="video/mp4" />
+      </video>
+    </div>
   );
 };
