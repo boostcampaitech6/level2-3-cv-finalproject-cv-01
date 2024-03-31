26ae4ee60946f68a8690652cd513d3495574f4a5
2018007956 <chaea11s0@gmail.com>
2024-03-31 16:29:59 +0000

    fix: host ip in db must be hardcoded because the server ip can set to localhost in dev environment #146

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,12 +4,12 @@ import os
 
 class Config(BaseSettings):
     pass_my: str = Field(os.getenv('DB_PASS'), env="DB_PASS")
-    host_my: str = Field(os.getenv('SERVER_IP'), env="SERVER_IP")
 
     @property
     def db_url(self) -> str:
         user_name = 'root'
+        host_my = '175.45.200.149'
         db_name = 'STOCK_DB'
-        return f"mysql+mysqlconnector://{user_name}:{self.pass_my}@{self.host_my}/{db_name}"
+        return f"mysql+mysqlconnector://{user_name}:{self.pass_my}@{host_my}/{db_name}"
     
 config = Config()
