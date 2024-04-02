7975ca4481b7ed85f92958274dd09ce308ece878
Chaea Kim <chaea11s0@gmail.com>
2024-04-03 02:24:50 +0900

    feat: 모델 종합 점수에 따라 주식짤 띄우기 (#151)
    * feat: add picture
    
    #148
    
    * feat: modify result page
    
    #148
    
    * fix: add port on result screen
    
    #148
    
    * feat: change favorite page UI
    
    #148
    
    * fix: port, result screen string to float
    
    #148
    
    * feat: change mobile intro file / add Google Analytics
    
    #148
    
    * feat: handle cachedata
    
    #148
    
    * fix: del handling cachedata-favorite page
    
    #148
    
    ---------
    
    Co-authored-by: JasonPark <cjpark137@naver.com>
    Co-authored-by: minyun-e <enuynim@gmail.com>

--- frontend/src/components/DivWrapper/DivWrapper.jsx
+++ frontend/src/components/DivWrapper/DivWrapper.jsx
@@ -6,14 +6,14 @@ Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcN
 import React from "react";
 import "./style.css";
 
-export const DivWrapper = ({ className, companyName, graph, symbol, price, change, volume, logo }) => {
+export const DivWrapper = ({ className, companyName, graph, symbol, price, change, volume, logo, onClick }) => {
   return (
-    <div className={`div-wrapper ${className}`}>
+    <div className={`div-wrapper ${className}`} onClick={onClick}>
       <div className="overlap-group-2">
         <div className="background">
           <div className="frame-2">
             <div className="text-wrapper-5">{price}</div>
-          </div>
+          
           <div className="frame-3">
             <div className="text-wrapper-6">{change}</div>
             <div className="text-wrapper-7">{volume}</div>
@@ -24,6 +24,7 @@ export const DivWrapper = ({ className, companyName, graph, symbol, price, chang
           <div className="text-wrapper-9">{symbol}</div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
--- frontend/src/screens/Loading/Loading.jsx
+++ frontend/src/screens/Loading/Loading.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import introVideoDesktop from "../../../dist/video/intro_desktop.mp4";
-import introVideoMobile from "../../../dist/video/intro_mobile.mp4";
+// import introVideoMobile from "../../../dist/video/intro_mobile.mp4";
+import introImageMobile from "../../../dist/video/intro_mobile.webp";
 import "./style.css";
 
 export const Loading = () => {
@@ -13,14 +14,24 @@ export const Loading = () => {
     }, 800);
   };
 
+  const handleImageLoad = () => {
+    setTimeout(() => {
+      navigate('/login');
+    }, 5010);
+  };
+
+
   return (
     <div className="video-container">
       <video autoPlay muted className="video desktop-video" onEnded={handleVideoEnd}>
         <source src={introVideoDesktop} type="video/mp4" />
       </video>
-      <video autoPlay muted className="video mobile-video" onEnded={handleVideoEnd}>
-        <source src={introVideoMobile} type="video/mp4" />
-      </video>
+      <img
+        src={introImageMobile}
+        alt="Intro"
+        className="video mobile-image" 
+        onLoad={handleImageLoad}
+      />
     </div>
   );
 };
