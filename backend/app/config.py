afeb53e0e40a540cef8380566c89e5ef73537034
2018007956 <chaea11s0@gmail.com>
2024-04-02 20:27:38 +0000

    fix: code bugs in back
    #146

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,11 +4,11 @@ import os
 
 class Config(BaseSettings):
     pass_my: str = Field(os.getenv('DB_PASS'), env="DB_PASS")
-    
+
     @property
     def db_url(self) -> str:
         user_name = 'root'
-        host_my = os.getenv('SERVER_IP')
+        host_my = '175.45.200.149'
         db_name = 'STOCK_DB'
         return f"mysql+mysqlconnector://{user_name}:{self.pass_my}@{host_my}/{db_name}"
     
